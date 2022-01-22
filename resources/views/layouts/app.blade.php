fa6e0debcf005c526ad64d545db9e13429aa78e0
albert huang <d0191512@gmail.com>
2022-01-23 01:06:14 +0800

    compile assect

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
         </title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         @include('layouts.css')
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
     </head>
     <body>
         @include('layouts.nav')
